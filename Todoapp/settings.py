639810db4d7b6307c2defe608e29549e8e606872
Gulshan kumar <kgulshan827@gmail.com>
2021-07-04 14:25:03 +0530

    deploy

--- Todoapp/settings.py
+++ Todoapp/settings.py
@@ -123,10 +123,7 @@ USE_TZ = True
 
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# Extra places for collectstatic to find static file
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
